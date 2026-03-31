9077e4b440b85563362207f1443e9e0ce7b9db22
rosamirandagustavo-droid <rosa.miranda.gustavo@escola.pr.gov.br>
2026-03-31 01:38:52 +0000

    Trabalho finalizado

---
+++ listaLivros.js
@@ -0,0 +1,28 @@
+const precosLivros = [
+  {
+    titulo: "JavaScript",
+    preco: 25
+  },
+  {
+    titulo: "PHP",
+    preco: 15
+  }, 
+  {
+    titulo: "Java",
+    preco: 30
+  }, 
+  {
+    titulo: "Elixir",
+    preco: 50
+  }, 
+  {
+    titulo: "Go",
+    preco: 45
+  }, 
+  {
+    titulo: "Física 1",
+    preco: 2
+  }
+];
+
+module.exports = precosLivros;
